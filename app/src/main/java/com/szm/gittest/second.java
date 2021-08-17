8fe3468efd97726c74c787a06872bb3418b5afcc
sunzhimin8 <694041878@qq.com>
2021-08-17 21:59:44 +0800

    01

--- app/src/main/java/com/szm/gittest/second.java
+++ app/src/main/java/com/szm/gittest/second.java
@@ -2,16 +2,13 @@ package com.szm.gittest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class second extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        startActivity(new Intent(this,second.class));
+        setContentView(R.layout.activity_second);
     }
 }
