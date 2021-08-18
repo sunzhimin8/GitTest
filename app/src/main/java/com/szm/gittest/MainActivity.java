932629898ac2abf0a661e76cf6be5434a5fe8c7b
sunzhimin8 <694041878@qq.com>
2021-08-18 11:13:49 +0800

    04 by szm

--- app/src/main/java/com/szm/gittest/MainActivity.java
+++ app/src/main/java/com/szm/gittest/MainActivity.java
@@ -4,14 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+  private Button btn_main;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btn_main=findViewById(R.id.btn_main);
 
+        btn_main.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        startActivity( new Intent(this,DBActivity.class));
     }
 }
